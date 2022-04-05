e66c07a57bf60f1127a507c32fa8d6caf92f8905
codebenderhq <lionelmbankeu@gmail.com>
2022-04-05 09:12:02 +0200

    updated template

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    "./node_modules/sauveur.js/.sdk/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/sauveur.js/core/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
